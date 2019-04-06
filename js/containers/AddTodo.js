66c4c88c9f8da5db1a2f393b8389c36fc3176210
Swifty Wang <wsk900906@gmail.com>
2019-04-07 00:35:02 +0800

    fine tune

--- js/containers/AddTodo.js
+++ js/containers/AddTodo.js
@@ -1,33 +1,44 @@
 /**
  * Created by zhongpeng on 2018/9/25.
  */
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {
-    View,
-    TextInput,
-    Button,
+  View,
+  TextInput,
+  Button,
 } from 'react-native'
-import { connect } from 'react-redux'
-import { addTodo } from '../actions'
+import {connect} from 'react-redux'
+import {addTodo} from '../actions'
+import {ToastAndroid} from 'react-native';
 
 class AddTodo extends Component {
-    constructor(props){
-        super(props);
-        this.inputValue = '';
-    }
 
-    render(){
-        let { dispatch } = this.props;
-        return (
-            <View style={{flexDirection: 'row'}}>
-                <TextInput
-                    style={{flex:1, borderWidth: 1, borderColor: '#cccccc', textAlign: 'center'}}
-                    onChangeText={text => this.inputValue = text}
-                />
-                <Button title="Add Todo" onPress={() => dispatch(addTodo(this.inputValue))}/>
-            </View>
-        )
+  constructor(props) {
+    super(props);
+    this.inputValue = '';
+    this.dispatchText = this.dispatchText.bind(this);
+  }
+
+  render() {
+
+    return (
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <TextInput
+          style={{flex: 1, textAlign: 'center'}}
+          onChangeText={text => this.inputValue = text}
+        />
+        <Button style={{height: 200}} title="Add to Todo" onPress={ this.dispatchText }/>
+      </View>
+    )
+  }
+
+  dispatchText() {
+    if (this.inputValue.trim() != '') {
+      this.props.dispatch(addTodo(this.inputValue));
+    } else {
+      ToastAndroid.show("input cannot be empty", ToastAndroid.SHORT);
     }
+  }
 }
 
 export default connect()(AddTodo)
